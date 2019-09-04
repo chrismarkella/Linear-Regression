f1638cc9bbc770170d8c365a4f91d33de01fa747
Krisztian Markella <chris.markella@gmail.com>
2019-09-03 21:59:04 -0700

    output fuctions are added to clean up a bit the code

--- linear_regression.py
+++ linear_regression.py
@@ -5,33 +5,58 @@ from sklearn.preprocessing import PolynomialFeatures
 x = np.array([16, 17, 23, 25, 28, 31, 40, 48, 54, 67]).reshape((-1, 1))
 y = np.array([180, 163, 172, 168, 165, 156, 153, 144, 139, 130])
 
-transformer = PolynomialFeatures(degree = 2, include_bias = False)
-transformer.fit(x)
-x_ = transformer.transform(x)
+def linear_regression(x, y):
+  model = LinearRegression().fit(x, y)
 
-model = LinearRegression().fit(x_, y)
+  coefficient_of_determination = model.score(x, y) # R^2
+  slope = model.coef_
+  interception = model.intercept_
 
-coefficient_of_determination = model.score(x_, y)
-interception = model.intercept_
-coefficients = model.coef_
+  return (coefficient_of_determination, slope, interception)
 
-prediction = lambda age: coefficients[0] * age**2 + coefficients[1]*age + interception
+def linear_regression_output(q_sq_2, slope, interception, prediction_function):
+  coefficient_of_determination = q_sq_2
 
+  print('The linear regression model is y = %.2f*x + %.2f' % (slope, interception))
 
-print('The quadratic regression model is y = %.4f*x^2 + %.4f*x + %.4f' % (coefficients[0], coefficients[1], interception))
-
-age = int(input('what is your age: '))
-print('your recomended HR is %d' % (prediction(age)))
-print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
-# model = LinearRegression().fit(x, y)
-
-# coefficient_of_determination = model.score(x, y) # R^2
-# slope = model.coef_
-# interception = model.intercept_
+  age = int(input('what is your age: '))
+  print('your recomended HR is %d' % (prediction_function(age)))
+  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
 
 
-# print('The linear regression model is y = %.2f*x + %.2f' % (slope, interception))
 
-# age = int(input('what is your age: '))
-# print('your recomended HR is %d' % (prediction(age)))
-# print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
+def quadratic_regression(x, y):
+  transformer = PolynomialFeatures(degree = 2, include_bias = False)
+  transformer.fit(x)
+  x_ = transformer.transform(x)
+
+  model = LinearRegression().fit(x_, y)
+  
+  coefficient_of_determination = model.score(x_, y)
+  interception = model.intercept_
+  coefficients = model.coef_
+
+  return (coefficient_of_determination, coefficients, interception)
+  
+def quadratic_regression_output(q_sq_2, coefficients, interception, prediction_function):
+  coefficient_of_determination = q_sq_2
+
+  print('The quadratic regression model is y = %.4f*x^2 + %.4f*x + %.4f' % (coefficients[0], coefficients[1], interception))
+
+  age = int(input('what is your age: '))
+  print('your recomended HR is %d' % (prediction(age)))
+  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
+
+
+# Linear regression
+coefficient_of_determination, slope, interception = linear_regression(x, y)
+prediction = lambda age: slope * age + interception
+
+linear_regression_output(coefficient_of_determination, slope, interception, prediction)
+
+
+# Quadratic regression
+coefficient_of_determination, coefficients, interception = quadratic_regression(x, y)
+prediction = lambda age: coefficients[-1] * age**2 + coefficients[-2]*age + interception
+
+quadratic_regression_output(coefficient_of_determination, coefficients, interception, prediction)
