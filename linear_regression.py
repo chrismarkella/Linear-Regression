1dc530883cb6668a91c0c1da89475d602bedfb63
Krisztian Markella <chris.markella@gmail.com>
2019-09-04 20:22:05 -0700

    Cleaned up a bit. There is a bit of a demo of the implemented functions in the main function.

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,6 @@
+
+# Used this tutorial: https://realpython.com/linear-regression-in-python/
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
@@ -6,73 +9,6 @@ from sklearn.preprocessing import PolynomialFeatures
 # https://johnstonmd.wordpress.com/teaching/math-178-spring-2017/
 x = np.array([16, 17, 23, 25, 28, 31, 40, 48, 54, 67]).reshape((-1, 1))
 y = np.array([180, 163, 172, 168, 165, 156, 153, 144, 139, 130])
-
-def linear_regression(x, y):
-  """
-  Return the coefficient of determination (R^2), slope and interception
-  """
-  model = LinearRegression().fit(x, y)
-
-  coefficient_of_determination = model.score(x, y) # R^2
-  slope = model.coef_
-  interception = model.intercept_
-
-  return (coefficient_of_determination, slope, interception)
-
-def linear_regression_reuse(x, y):
-  """
-  >>> linear_regression(x, y) == linear_regression_reuse(x, y)
-  True
-  """
-  # Reusing the existing 'general' polynomial_regression function
-  return polynomial_regression(x, y, 1) 
-  
-
-def linear_regression_output(q_sq_2, slope, interception, prediction_function):
-  coefficient_of_determination = q_sq_2
-
-  print('The linear regression model is y = %.2f*x + %.2f' % (slope, interception))
-
-  age = int(input('what is your age: '))
-  print('your recomended HR is %d' % (prediction_function(age)))
-  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
-
-
-
-def quadratic_regression(x, y):
-  transformer = PolynomialFeatures(degree = 2, include_bias = False)
-  transformer.fit(x)
-  x_ = transformer.transform(x)
-
-  model = LinearRegression().fit(x_, y)
-  
-  coefficient_of_determination = model.score(x_, y)
-  interception = model.intercept_
-  coefficients = model.coef_
-
-  return (coefficient_of_determination, coefficients, interception)
-  
-def quadratic_regression_reuse(x, y):
-  """
-  In the testing splitting up the return values to START, MID, END
-  then compairing the values one by one.
-  NOTICE: quadratic_regression(x, y) == quadratic_regression_reuse(x, y) will throw and EXCEPTION!
-  >>> start1, *mid1, end1 = quadratic_regression(x, y)
-  >>> start2, *mid2, end2 = quadratic_regression_reuse(x, y)
-  >>> start1 == start2 and np.array_equal(mid1, mid2) and end1 == end2
-  True
-  """
-  return polynomial_regression(x, y, 2)
-
-def quadratic_regression_output(q_sq_2, coefficients, interception, prediction_function):
-  coefficient_of_determination = q_sq_2
-
-  print('The quadratic regression model is y = %.4f*x^2 + %.4f*x + %.4f' % (coefficients[-1], coefficients[-2], interception))
-  # print('The quadratic regression model is y = %.4f*x + %.4f*x^2 + %.4f' % (coefficients[0] , coefficients[1] , interception))
-
-  age = int(input('what is your age: '))
-  print('your recomended HR is %d' % (prediction(age)))
-  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
 
 def polynomial_regression(x, y, degree_my = 1):
   transformer = PolynomialFeatures(degree = degree_my, include_bias = False)
@@ -87,28 +23,43 @@ def polynomial_regression(x, y, degree_my = 1):
 
   return (coefficient_of_determination, coefficients, interception)
 
-def cubic_regression(x, y):
-  transformer = PolynomialFeatures(degree = 3, include_bias = False)
-  transformer.fit(x)
-  x_ = transformer.transform(x)
-
-  model = LinearRegression().fit(x_, y)
-  
-  coefficient_of_determination = model.score(x_, y)
-  interception = model.intercept_
-  coefficients = model.coef_
-
-  return (coefficient_of_determination, coefficients, interception)
-
-def cubic_regression_reuse(x, y):
+def linear_regression(x, y):
   """
-  In the testing splitting up the return values to START, MID, END
-  then compairing the values one by one.
-  NOTICE: cubic_regression(x, y) == cubic_regression_reuse(x, y) will throw and EXCEPTION!
-  >>> start1, *mid1, end1 = cubic_regression(x, y)
-  >>> start2, *mid2, end2 = cubic_regression_reuse(x, y)
-  >>> start1 == start2 and np.array_equal(mid1, mid2) and end1 == end2
-  True
+  Returns R^2, slope and interception
+  """
+  # Reusing the existing 'general' polynomial_regression function
+  return polynomial_regression(x, y, 1) 
+  
+
+def linear_regression_output(q_sq_2, slope, interception, prediction_function):
+  coefficient_of_determination = q_sq_2
+
+  print('The linear regression model is y = %.2f*x + %.2f' % (slope, interception))
+
+  age = int(input('what is your age: '))
+  print('your recomended HR is %d' % (prediction_function(age)))
+  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
+
+def quadratic_regression(x, y):
+  """
+  Returning R^2, coefficients and the interceptions as a size three tupple.
+  """
+  return polynomial_regression(x, y, 2)
+
+def quadratic_regression_output(q_sq_2, coefficients, interception, prediction_function):
+  coefficient_of_determination = q_sq_2
+
+  print('The quadratic regression model is y = %.4f*x^2 + %.4f*x + %.4f' % (coefficients[-1], coefficients[-2], interception))
+  # print('The quadratic regression model is y = %.4f*x + %.4f*x^2 + %.4f' % (coefficients[0] , coefficients[1] , interception))
+
+  age = int(input('what is your age: '))
+  print('your recomended HR is %d' % (prediction_function(age)))
+  print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
+
+
+def cubic_regression(x, y):
+  """
+  Returning R^2, coefficients and the interceptions as a size three tupple.
   """
   return polynomial_regression(x, y, 3)
 
@@ -118,7 +69,7 @@ def cubic_regression_output(q_sq_2, coefficients, interception, prediction_funct
   print('The cubic regression model is y = %.4f*x^3 + %.4f*x^2 + %.4f*x + %.4f' % (coefficients[0], coefficients[1], coefficients[2], interception))
 
   age = int(input('what is your age: '))
-  print('your recomended HR is %d' % (prediction(age)))
+  print('your recomended HR is %d' % (prediction_function(age)))
   print('this is %.2f percent accurate' %( coefficient_of_determination * 100) )
 
 
@@ -147,8 +98,23 @@ def top_polynomial_regressions(x, y, max_degree, top = 10):
     print('%.4f Q^2 with degree %d' %(regression[0], regression[1]))
 
 
-if __name__ == "__main__":
-  print('\n'*20, 'All tests passed')
 
-  import doctest
-  doctest.testmod()
+
+if __name__ == "__main__":
+
+  #  '\' is just for to be able to break the line and tabulate nice
+  functions = \
+  [
+    (linear_regression,    linear_regression_output   ),
+    (quadratic_regression, quadratic_regression_output),
+    (cubic_regression,     cubic_regression_output    )
+  ]
+
+  for f_in, f_out in functions:
+    r_sq, coefficients, interception = f_in(x,y)
+    prediction = \
+      lambda age: sum( [coefficients[i] * age**(i + 1) for i in range(len(coefficients))] ) + interception
+  
+    f_out(r_sq, coefficients, interception, prediction)
+
+  top_polynomial_regressions(x, y, 150, 5)
